6c919ba721b4f3f834e64e3c0d3cc6a6ca6308d3
izaack55182 <L20TE0298@TEPEACA.TECNM.MX>
2026-02-05 14:16:25 -0600

    Eliminacion de textos y cambio a fondo transparente de QR de la seccion LinkGenerator

--- src/components/LinkGenerator.tsx
+++ src/components/LinkGenerator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import QRCodeStyling from 'qr-code-styling';
 import { employees } from '../data/employees';
